753721ab1f09305ba50358e3d9236c3739a3ca9b
shanalotte17 <122484737+shanalotte17@users.noreply.github.com>
2023-01-12 01:21:55 +0500

    123
    123

--- src/main/java/net/anna/testmod/TestMod.java
+++ src/main/java/net/anna/testmod/TestMod.java
@@ -10,6 +10,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+
+//samplecomment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TestMod.MOD_ID)
 public class TestMod
