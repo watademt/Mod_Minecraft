d840917220add046e2ee9f80c01efce3042c5bd2
shanalotte <katsushinigami@gmail.com>
2023-01-17 01:08:49 +0500

    added fullerine and fullerite

--- src/main/java/net/anna/testmod/items/ModCreativeModeTab.java
+++ src/main/java/net/anna/testmod/items/ModCreativeModeTab.java
@@ -10,4 +10,6 @@ public class ModCreativeModeTab {
             return new ItemStack(ModItems.OSMIUM.get());
         }
     };
+
+
 }
--- src/main/java/net/anna/testmod/items/ModItems.java
+++ src/main/java/net/anna/testmod/items/ModItems.java
@@ -13,6 +13,9 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
 
     public static final RegistryObject<Item> OSMIUM = ITEMS.register("osmium", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+    public static final RegistryObject<Item> FULLERINE = ITEMS.register("fullerine", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+    public static final RegistryObject<Item> FULLERITE = ITEMS.register("fullerite", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
 
     //add fullerite alloy ingot,
     // osmium, osmium ore
